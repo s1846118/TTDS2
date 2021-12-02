bc4170e2e5626db0c1941dbeb1e9549061100cd1
Adam Henderson <s1846118@ed.ac.uk>
2021-12-02 13:32:18 +0000

    MI and chi working

--- code.py
+++ code.py
@@ -4,6 +4,7 @@ import re
 import pandas as pd
 from nltk import text
 from nltk.stem import PorterStemmer
+import math
 
 class eval():
 
@@ -121,18 +122,75 @@ class text_eval():
             self.corpus_df.iloc[row]['Verse'] = [ps.stem(word).lower() for word in self.corpus_df.iloc[row]['Verse']] # Porter stemming
 
     def MICHI(self):
-        pass
+        self.tokens = []
+
+        for x in self.corpus_df['Verse']:
+            self.tokens += x
+
+        self.tokens = list(set(self.tokens))
+        self.michi = {} # Dictionary where each key is the token and contains a dict where keys are corpus and values are MI(index 0) CHI(index 1)
+        
+        for i, token in enumerate(self.tokens):
+            self.michi[token] = {}
+            for corpus in list(set(self.corpus_df['Corpus'])):
+                    MI = 0
+                    chi = 0
+                    print(i)
+                    self.michi[token][corpus] = {}
+                    corp_docs = self.corpus_df[self.corpus_df['Corpus']==corpus]['Verse']
+                    non_corp_docs = self.corpus_df[self.corpus_df['Corpus']!=corpus]['Verse']
+
+                    N, N11, N10, N01, N00 = 1, 1, 1, 1, 1
+
+                    N = len(self.corpus_df['Corpus']) # Total number of documents. Stays constant.
+                    N11 = len([1 for x in corp_docs if token in x])
+                    N10 = len([1 for x in non_corp_docs if token in x])
+                    N01 = len([1 for x in corp_docs if token not in x])
+                    N00 = len([1 for x in non_corp_docs if token not in x])
+
+                    try:
+                        MI = (N11/N)*math.log((N*N11)/((N11+N10)*(N11+N01)),2)
+                        + (N01/N)*math.log((N*N01)/((N01+N00)*(N11+N01)),2)
+                        + (N10/N)*math.log((N*N10)/((N10+N11)*(N00+N10)),2)
+                        + (N00/N)*math.log((N*N00)/((N10+N00)*(N01+N00)),2)
+                    except ValueError:
+                        self.michi[token][corpus]['MI'] = 0
+                    try:
+                        E00 = N*((N00+N10)/N)*((N00+N01)/N)
+                        E01 = N*((N01+N11)/N)*((N01+N00)/N)
+                        E10 = N*((N10+N11)/N)*((N10+N00)/N)
+                        E11 = N*((N11+N10)/N)*((N11+N01)/N)
+
+                        chi = ((N11-E11)**2)/E11 + ((N00-E00)**2)/E00 + ((N01-E01)**2)/E01 + ((N10-E10)**2)/E10 
+                    except:
+                        chi = 0
+
+                    self.michi[token][corpus]['MI'] = MI
+                    self.michi[token][corpus]['chi'] = chi
+
+    # N11 (number of documents that contain the term and are in corpus 1)
+    # N10 (number of documents that contain the term and are in corpus 2).
+    # N01 (number of documents that do not contain the term and are in corpus 1)
+    # N00 (number of documents that do not contain the term and are in corpus 2)
 
 
 def main():
     # evl = eval('system_results.csv', 'qrels.csv')
     # evl.create_index()
     # evl.stats()
-    # print(evl.index)
-
+    # hello = str(evl.index)
+    # print(hello)
+    
     t_evl = text_eval("train_and_dev.tsv", stopwords='../../Collections/stopwords.txt')
     t_evl.pre_process()
-    print(t_evl.corpus_df)
+    t_evl.MICHI()
+    print(t_evl.michi)
+
+    with open('michi.txt','w') as f:
+        f.write(str(t_evl.michi))
+        f.close()
+
+
 
 
 
