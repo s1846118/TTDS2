88a1997bc24b2b6cddcd72151eadf098d5953d05
Adam Henderson <s1846118@ed.ac.uk>
2021-12-03 22:25:16 +0000

    Task 3 almost complete, enough 4 now

--- code.py
+++ code.py
@@ -1,3 +1,4 @@
+from nltk.util import transitive_closure
 from numpy import genfromtxt
 import numpy as np
 import re
@@ -5,6 +6,14 @@ import pandas as pd
 from nltk import text
 from nltk.stem import PorterStemmer
 import math
+import csv
+from pandas.io.parsers import read_csv
+from sklearn.model_selection import train_test_split
+from sklearn.utils.extmath import cartesian
+from sklearn import svm
+import scipy
+
+
 
 class eval():
 
@@ -103,14 +112,14 @@ class text_eval():
                 lst = re.split(Pattern, doc)
                 self.stopwords = [elem for elem in lst if elem != '']
 
-        self.corpus_df = pd.read_csv(path,'\t')
+        self.corpus_df = pd.read_csv(path,'\t', quoting=csv.QUOTE_NONE)
         line = []
         line.insert(0, {self.corpus_df.columns.values[0]: self.corpus_df.columns.values[0], self.corpus_df.columns.values[1]: self.corpus_df.columns.values[1]})
         # concatenate two dataframe
         self.corpus_df = pd.concat([pd.DataFrame(line),self.corpus_df], ignore_index=True)
         self.corpus_df = self.corpus_df.rename(columns={self.corpus_df.columns.values[0]:'Corpus', self.corpus_df.columns.values[1]:'Verse'})
 
-    def pre_process(self): # Preprocess data
+    def pre_process(self, pstem = False): # Preprocess data
 
         ps = PorterStemmer()
 
@@ -119,7 +128,8 @@ class text_eval():
             self.corpus_df.iloc[row]['Verse'] = re.split('[^A-Z^a-z\d]', self.corpus_df.iloc[row]['Verse']) 
             self.corpus_df.iloc[row]['Verse'] = [elem.lower() for elem in self.corpus_df.iloc[row]['Verse'] if elem != ''] # Tokenisation
             self.corpus_df.iloc[row]['Verse'] = [elem for elem in self.corpus_df.iloc[row]['Verse'] if not(elem in self.stopwords)] # Remove stopwords
-            self.corpus_df.iloc[row]['Verse'] = [ps.stem(word).lower() for word in self.corpus_df.iloc[row]['Verse']] # Porter stemming
+            if pstem == True:
+                self.corpus_df.iloc[row]['Verse'] = [ps.stem(word).lower() for word in self.corpus_df.iloc[row]['Verse']] # Porter stemming
 
     def MICHI(self):
         self.tokens = []
@@ -140,38 +150,132 @@ class text_eval():
                     corp_docs = self.corpus_df[self.corpus_df['Corpus']==corpus]['Verse']
                     non_corp_docs = self.corpus_df[self.corpus_df['Corpus']!=corpus]['Verse']
 
-                    N, N11, N10, N01, N00 = 1, 1, 1, 1, 1
-
-                    N = len(self.corpus_df['Corpus']) # Total number of documents. Stays constant.
+                    N = self.corpus_df.shape[0] # Total number of documents. Stays constant.
                     N11 = len([1 for x in corp_docs if token in x])
                     N10 = len([1 for x in non_corp_docs if token in x])
                     N01 = len([1 for x in corp_docs if token not in x])
                     N00 = len([1 for x in non_corp_docs if token not in x])
 
                     try:
-                        MI = (N11/N)*math.log((N*N11)/((N11+N10)*(N11+N01)),2)
-                        + (N01/N)*math.log((N*N01)/((N01+N00)*(N11+N01)),2)
-                        + (N10/N)*math.log((N*N10)/((N10+N11)*(N00+N10)),2)
-                        + (N00/N)*math.log((N*N00)/((N10+N00)*(N01+N00)),2)
+                        MI = (N11/N)*math.log((N*N11)/((N11+N10)*(N11+N01)),2)+ (N01/N)*math.log((N*N01)/((N01+N00)*(N11+N01)),2)+ (N10/N)*math.log((N*N10)/((N10+N11)*(N00+N10)),2)+ (N00/N)*math.log((N*N00)/((N10+N00)*(N01+N00)),2)
                     except ValueError:
                         self.michi[token][corpus]['MI'] = 0
                     try:
-                        E00 = N*((N00+N10)/N)*((N00+N01)/N)
-                        E01 = N*((N01+N11)/N)*((N01+N00)/N)
-                        E10 = N*((N10+N11)/N)*((N10+N00)/N)
-                        E11 = N*((N11+N10)/N)*((N11+N01)/N)
+                        if N11 == 0:
+                            chi = 0
+                        else:
+                            E00 = N*((N00+N10)/N)*((N00+N01)/N)
+                            E01 = N*((N01+N11)/N)*((N01+N00)/N)
+                            E10 = N*((N10+N11)/N)*((N10+N00)/N)
+                            E11 = N*((N11+N10)/N)*((N11+N01)/N)
 
-                        chi = ((N11-E11)**2)/E11 + ((N00-E00)**2)/E00 + ((N01-E01)**2)/E01 + ((N10-E10)**2)/E10 
+                            chi = ((N11-E11)**2)/E11 + ((N00-E00)**2)/E00 + ((N01-E01)**2)/E01 + ((N10-E10)**2)/E10 
                     except:
                         chi = 0
 
                     self.michi[token][corpus]['MI'] = MI
                     self.michi[token][corpus]['chi'] = chi
 
-    # N11 (number of documents that contain the term and are in corpus 1)
-    # N10 (number of documents that contain the term and are in corpus 2).
-    # N01 (number of documents that do not contain the term and are in corpus 1)
-    # N00 (number of documents that do not contain the term and are in corpus 2)
+class text_classifier():
+
+    def __init__(self, corpus):
+
+        self.corpus = corpus
+
+    def pre_process(self, pstem = False): # Preprocess data
+
+        ps = PorterStemmer()
+
+        for row in range(self.corpus_df.shape[0]):
+
+            self.corpus_df.iloc[row]['Verse'] = re.split('[^A-Z^a-z\d]', self.corpus_df.iloc[row]['Verse']) 
+            self.corpus_df.iloc[row]['Verse'] = [elem.lower() for elem in self.corpus_df.iloc[row]['Verse'] if elem != ''] # Tokenisation
+            self.corpus_df.iloc[row]['Verse'] = [elem for elem in self.corpus_df.iloc[row]['Verse'] if not(elem in self.stopwords)] # Remove stopwords
+            if pstem == True:
+                self.corpus_df.iloc[row]['Verse'] = [ps.stem(word).lower() for word in self.corpus_df.iloc[row]['Verse']] # Porter stemming
+
+    def get_vocab(self, corpus):
+
+        vocab = []
+        title = []
+
+        for verse in corpus['Verse']:
+            vocab+=verse
+
+        for tit in corpus['Corpus']:
+            title.append(tit)
+
+        words = list(set(vocab))
+        titles = list(set(title))
+
+
+        return words, titles
+
+
+    def word_cat_2ID(self,words, titles):
+        # convert the vocab to a word id lookup dictionary
+        # anything not in this will be considered "out of vocabulary" OOV
+        word2id = {}
+        for word_id,word in enumerate(words):
+            word2id[word] = word_id
+            
+        # and do the same for the categories
+        cat2id = {}
+        for cat_id,cat in enumerate(titles):
+            cat2id[cat] = cat_id
+
+        self.word_id = word2id
+        self.cat_id = cat2id
+
+        return word2id, cat2id
+
+    def shuffle_and_split(self,corpus):
+
+        shuffled_corpus = corpus.sample(frac=1) # Shuffles the dataframe
+
+        Xtrain, Xtest = train_test_split(shuffled_corpus, test_size=0.2)
+
+        return Xtrain, Xtest
+
+    def convert_to_bow(self,corpus,word2id):
+
+        # matrix size is number of docs x vocab size + 1 (for OOV)
+        matrix_size = (len(corpus['Verse']),len(word2id)+1)
+        oov_index = len(word2id)
+        # matrix indexed by [doc_id, token_id]
+        X = scipy.sparse.dok_matrix(matrix_size)
+
+        # iterate through all documents in the dataset
+        for doc_id,doc in enumerate(corpus['Verse']):
+            for word in doc:
+                # default is 0, so just add to the count for this word in this doc
+                # if the word is oov, increment the oov_index
+                X[doc_id,word2id.get(word,oov_index)] += 1
+        
+        return X
+
+    def catID(self,categories, cat2id):
+
+        return [cat2id[x] for x in categories]
+
+    def train_model(self, BOW_matrix, y_train, c):
+
+        model = svm.LinearSVC(C=c)
+        # then train the model!
+        model.fit(BOW_matrix,y_train)
+
+        return model
+
+    def compute_accuracy(self, predictions, true_values):
+        num_correct = 0
+        num_total = len(predictions)
+        for predicted,true in zip(predictions,true_values):
+            if predicted==true:
+                num_correct += 1
+        return num_correct / num_total
+
+    # def compute_stats(self, predictions, true_output:
+
 
 
 def main():
@@ -180,17 +284,55 @@ def main():
     # evl.stats()
     # hello = str(evl.index)
     # print(hello)
+
+    testing = text_eval('test.tsv', stopwords='../../Collections/stopwords.txt')
+    testing.pre_process()
+    testing_corpus = testing.corpus_df.copy(deep=True)
     
     t_evl = text_eval("train_and_dev.tsv", stopwords='../../Collections/stopwords.txt')
     t_evl.pre_process()
-    t_evl.MICHI()
-    print(t_evl.michi)
 
-    with open('michi.txt','w') as f:
-        f.write(str(t_evl.michi))
-        f.close()
+    corpus_data = t_evl.corpus_df.copy(deep=True)
 
+    print(corpus_data)
 
+    text_class = text_classifier(corpus_data)
+
+    print(text_class.corpus)
+
+    vocab,titles = text_class.get_vocab(text_class.corpus)
+
+    wordID, catID = text_class.word_cat_2ID(vocab,titles)
+
+    Xtrain, Xtest = text_class.shuffle_and_split(text_class.corpus)
+
+    BOW_matrix_train = text_class.convert_to_bow(Xtrain, wordID)
+
+    BOW_matrix_test = text_class.convert_to_bow(Xtest, wordID)
+
+    BOW_matrix_test2 = text_class.convert_to_bow(testing_corpus, wordID)
+
+    print(catID)
+
+    Ytrain = text_class.catID(Xtrain['Corpus'], catID)
+
+    Ytest = text_class.catID(Xtest['Corpus'], catID)
+
+    Ytest2 = text_class.catID(testing_corpus['Corpus'], catID)
+
+    model = text_class.train_model(BOW_matrix_train,Ytrain, 3)
+
+    predictions = model.predict(BOW_matrix_train)
+
+    print(text_class.compute_accuracy(predictions,Ytrain))
+
+    predictions = model.predict(BOW_matrix_test)
+
+    print(text_class.compute_accuracy(predictions,Ytest))
+
+    predictions = model.predict(BOW_matrix_test2)
+
+    print(text_class.compute_accuracy(predictions,Ytest2))
 
 
 
