1e3989846460ecaa6abb4e0979d86032f3e3a302
Adam Henderson <s1846118@ed.ac.uk>
2021-12-05 20:51:28 +0000

    Everything working bar LDA and DCG

--- code.py
+++ code.py
@@ -41,6 +41,7 @@ class eval():
         for system in range(self.systems):
             for query in range(self.queries):
                 relQ = self.q_np[self.q_np[:,0] == query+1,:]
+
                 # First x results for each system for each query
                 results = self.sys_np[self.sys_np[:,0] == system+1,:]
                 results10 = results[results[:,1] == query+1,:][:10,:]
@@ -67,42 +68,52 @@ class eval():
                 if len(precisions) == 0:
                     AvgP = 0
                 else:
-                    AvgP = sum(precisions)/len(precisions)
+                    AvgP = sum(precisions)/relQ.shape[0]
 
                 # Discounted Cumulative Gain
                 DCG20 = 0
                 iDGC = 0
+
+                print(np.sum(relQ[:,2]))
+
                 for i, r in enumerate(results20[:,2]):
                     if i == 0:
-                        iDGC = 1       
+                        iDGC = relQ[i,2]
                         if len(np.where(relQ[:,1]==r)[0]) != 0:
-                            DCG20 = relQ[np.where(relQ[:,1]==r)[0],2][0]
+                            DCG20 = relQ[np.where(relQ[:,1]==r)[0],2][0]      
                         else:
                             DCG20 = 0
                     else:
-                        # iDGC+=3/np.log2(i+1)
                         if len(np.where(relQ[:,1]==r)[0]) != 0:
                             DCG20 += relQ[np.where(relQ[:,1]==r)[0],2][0]/np.log2(i+1)
-                            iDGC+= relQ[np.where(relQ[:,1]==r)[0],2][0]
-                            print(relQ[np.where(relQ[:,1]==r)[0],2][0])
-                            if i == 9:
-                                if DCG20 == 0:
-                                    nDCG10 = 0
-                                else:
-                                    nDCG10 = DCG20/(iDGC)
-                                self.index[system+1][query+1]['nDCGa10'] = round(nDCG10, 3)
+
+                            if i+1 > relQ.shape[0]:
+                                iDGC += 0
+                            else:
+                                iDGC+= relQ[i,2]
                         else:
                             DCG20 += 0
+                            if i+1 > relQ.shape[0]:
+                                iDGC+=0
+                            else:
+                                iDGC+=relQ[i,2]
 
-                if DCG20 == 0:
-                    nDCG20 = 0
-                else:
-                    nDCG20 = DCG20/(iDGC)
+                    if i == 9: # We must assign iDCG10
+                        nDCG10 = DCG20/(iDGC)
+                        self.index[system+1][query+1]['nDCGa10'] = round(nDCG10, 3)
+
+                    if i == 19:
+                        if DCG20 == 0:
+                            nDCG20 = 0
+                        else:
+                            nDCG20 = DCG20/(iDGC)
+                        
+                    
                 
                 self.index[system+1][query+1]['Pa10'] = RaR10/10
                 self.index[system+1][query+1]['Ra50'] = round(RaR50/len(relQ),3)
                 self.index[system+1][query+1]['r-precision'] = round(RaRR/len(relQ),3)
-                self.index[system+1][query+1]['AP'] = round(AvgP, 2)
+                self.index[system+1][query+1]['AP'] = round(AvgP, 3)
                 self.index[system+1][query+1]['nDCGa20'] = round(nDCG20, 3)
 
     def write_to_csv(self):
@@ -159,6 +170,8 @@ class text_eval():
     def MICHI(self):
         self.tokens = []
 
+        nvals = ""
+
         for x in self.corpus_df['Verse']:
             self.tokens += x
 
@@ -166,6 +179,8 @@ class text_eval():
         self.corpussies = list(set(self.corpus_df['Corpus']))
         self.michi = {} # Dictionary where each key is the token and contains a dict where keys are corpus and values are MI(index 0) CHI(index 1)
         
+        print(len(self.tokens))
+
         for i, token in enumerate(self.tokens):
             self.michi[token] = {}
             for corpus in list(set(self.corpus_df['Corpus'])):
@@ -181,6 +196,9 @@ class text_eval():
                 N10 = len([1 for x in non_corp_docs if token in x])
                 N01 = len([1 for x in corp_docs if token not in x])
                 N00 = len([1 for x in non_corp_docs if token not in x])
+
+                nvals += str(token) + ", " + str(corpus) +  ", "  + "N: " + str(N) + ", " + "N11: " + str(N11) + ", " + "N10: " + str(N10) + ", " + "N01: " + str(N01) + ", " + "N00: " + str(N00) + "\n"
+
 
                 try:
                     one = (N11/N)*math.log((N*N11)/((N11+N10)*(N11+N01)),2)
@@ -201,27 +219,33 @@ class text_eval():
 
                 MI = one+two+three+four
 
-                try:
-                    E00 = N*((N00+N10)/N)*((N00+N01)/N)
-                except:
-                    one = 0
-                try:
-                    E01 = N*((N01+N11)/N)*((N01+N00)/N)
-                except:
-                    two = 0
-                try:
-                    E10 = N*((N10+N11)/N)*((N10+N00)/N)
-                except:
-                    three = 0
-                try:
-                    E11 = N*((N11+N10)/N)*((N11+N01)/N)
-                except:
-                    four = 0
+                E00 = N*((N00+N10)/N)*((N00+N01)/N)
+                E01 = N*((N01+N11)/N)*((N01+N00)/N)
+                E10 = N*((N10+N11)/N)*((N10+N00)/N)
+                E11 = N*((N11+N10)/N)*((N11+N01)/N)
+                
+                # try:
+                #     one = ((N11-E11)**2)/E11
+                # except:
+                #     one = 0
+                # try:
+                #     two = ((N01-E01)**2)/E01
+                # except:
+                #     two = 0
+                # try:
+                #     three = ((N10-E10)**2)/E10
+                # except:
+                #     three = 0
 
-                    chi = one + two + three + four
+                chi =  ((N11-E11)**2)/E11 + ((N01-E01)**2)/E01 + ((N10-E10)**2)/E10
+                
 
                 self.michi[token][corpus]['MI'] = round(MI,3)
                 self.michi[token][corpus]['chi'] = round(chi,3)
+
+        with open('RoselynYUMMYMUMMY.txt', 'w') as f:
+            f.write(nvals)
+            f.close()
 
 
     def michi_to_csv(self, file):
@@ -410,29 +434,31 @@ class text_classifier():
 
 
 def main():
-    # evl = eval('system_results.csv', 'qrels.csv')
-    # evl.create_index()
-    # evl.stats()
-    # evl.write_to_csv()
- 
-    # parse_csv = text_eval.michi_to_csv('michi.txt')
+
+    ############# TASK 3 #############################
+
+    evl = eval('system_results.csv', 'qrels.csv')
+    evl.create_index()
+    evl.stats()
+    evl.write_to_csv()
+
+    ################ END #################################
+
+    ################ TASK2 ##############################
+    
+    # t_evl = text_eval("train_and_dev.tsv", stopwords='../../Collections/stopwords.txt')
+    # t_evl.pre_process(pstem=True)
+    # print(t_evl.corpus_df)
+
+    # t_evl.MICHI()
 
     
+    # with open('michi.txt', 'w') as f:
+    #     f.write(str(t_evl.michi))
 
-    # testing = text_eval('test.tsv', stopwords='../../Collections/stopwords.txt')
-    # testing.pre_process()
-    # testing_corpus = testing.corpus_df.copy(deep=True)
-    
-    t_evl = text_eval("train_and_dev.tsv", stopwords='../../Collections/stopwords.txt')
-    t_evl.pre_process()
-    print(t_evl.corpus_df)
+    # t_evl.michi_to_csv('michi.txt')
 
-    t_evl.MICHI()
-    
-    with open('michi.txt', 'w') as f:
-        f.write(str(t_evl.michi))
-
-    t_evl.michi_to_csv('michi.txt')
+    ################ END ##############################
 
     # corpus_data = t_evl.corpus_df.copy(deep=True)
 
